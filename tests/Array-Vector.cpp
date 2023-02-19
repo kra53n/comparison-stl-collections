205f066b3b456d7a959eb4b63448213d204ad908
TrondinDS <99561627+TrondinDS@users.noreply.github.com>
2023-02-19 18:14:16 +0300

    Добавление сравнения
    AA
    VV
    AV

--- tests/Array-Vector.cpp
+++ tests/Array-Vector.cpp
@@ -3,6 +3,7 @@
 #include <array>
 #include <algorithm>
 #include <chrono>
+#include <thread>
 
 // Function to sort a given vector
 double sortVector(std::vector<int>& vec, int i) {
@@ -24,12 +25,40 @@ double sortArray(std::array<int, 100000>& arr, int i) {
     return diff.count();
 }
 
+bool value—omparison(std::vector<int>& vec, std::array<int, 100000>& arr, int i) {
+    auto start = std::chrono::high_resolution_clock::now();
+    bool is_equal = std::equal(vec.begin(), vec.end(), arr.begin(), arr.end());
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> diff = end - start;
+    std::cout << i <<  "Time Comprarison: " << diff.count() << is_equal << std::endl;
+    return 0;
+}
+
+bool value—omparisonVvV(std::vector<int>& vec1, std::vector<int>& vec2, int i) {
+    auto start = std::chrono::high_resolution_clock::now();
+    bool is_equal = std::equal(vec1.begin(), vec1.end(), vec2.begin(), vec2.end());
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> diff = end - start;
+    std::cout << i << "Time Comprarison VvV: " << diff.count() << " " << is_equal << std::endl;
+    return 0;
+}
+
+bool value—omparisonAvA(std::array<int, 100000>& arr1, std::array<int, 100000>& arr2, int i) {
+    auto start = std::chrono::high_resolution_clock::now();
+    bool is_equal = std::equal(arr1.begin(), arr1.end(), arr2.begin(), arr2.end());
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> diff = end - start;
+    std::cout << i << "Time ComprarisonAvA: " << diff.count() << " " << is_equal << std::endl;
+    return 0;
+}
+
 int main() {
     
     double sumV = 0;
     double sumA = 0;
     for (int i = 0; i < 50; i++)
     {
+        std::this_thread::sleep_for(std::chrono::nanoseconds(500000000));
         std::vector<int> vec;
         std::array<int, 100000> arr;
         // Generate 100,000 random values and store them in the vector and array
@@ -38,10 +67,13 @@ int main() {
             vec.push_back(random_num);
             arr[i] = random_num;
         }
-
+        value—omparison(vec,arr,i);
+        value—omparisonVvV(vec, vec, i);
+        value—omparisonAvA(arr, arr, i);
         // Sort the vector and array
         sumV += sortVector(vec, i);
         sumA += sortArray(arr, i);
+
     }
     std::cout << "Result Time taken to sort the vector: " << sumV/50 << "s" << std::endl;
     std::cout << "Result Time taken to sort the array: " << sumA/50 << "s" << std::endl;
