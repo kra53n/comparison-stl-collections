7fa951e32372245eb679ead0fc132e12273d9dcf
Krai53n <greasha46@gmail.com>
2023-02-23 23:12:08 +0300

    add `time` function

--- tests/map-unordered_map.cpp
+++ tests/map-unordered_map.cpp
@@ -1,2 +1,19 @@
+#include <chrono>
+#include <iostream>
+
+using namespace std;
+
+int time(void (*func)(void)) {
+  auto start = chrono::steady_clock::now();
+  func();
+  auto end = chrono::steady_clock::now();
+  return (end - start) / 1ms;
+}
+
+void hello() {
+  cout << "hello" << endl;
+}
+
 int main() {
+  cout << time(hello) << endl;
 }
