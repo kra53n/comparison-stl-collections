1434eb551d0235c30d8cc8426d27ef4af612831a
Krai53n <greasha46@gmail.com>
2023-02-24 00:40:38 +0300

    start working on `forward_iteration` function

--- tests/map-unordered_map.cpp
+++ tests/map-unordered_map.cpp
@@ -1,19 +1,36 @@
+#include <map>
 #include <chrono>
 #include <iostream>
 
 using namespace std;
 
-int time(void (*func)(void)) {
-  auto start = chrono::steady_clock::now();
-  func();
-  auto end = chrono::steady_clock::now();
-  return (end - start) / 1ms;
-}
+namespace t_map {
 
-void hello() {
-  cout << "hello" << endl;
-}
+  map<int, int> create_map(int num, int seed) {
+	srand(seed);
+	map<int, int> m;
+	for (int i = 0; i < num; i++) {
+	  int tmp = rand();
+	  m[tmp] = tmp;
+	}
+	return m;
+  }
+
+  int forward_iteration(int num, int seed) {
+	auto m = create_map(num, seed);
+	auto start = chrono::steady_clock::now();
+	auto end = chrono::steady_clock::now();
+	return (end - start) / 1ms;
+  }
+
+}; // t_map
+
+namespace t_umap {
+
+
+
+}; // t_umap
 
 int main() {
-  cout << time(hello) << endl;
+  cout << t_map::forward_iteration(10'000'000, 333) << endl;
 }
