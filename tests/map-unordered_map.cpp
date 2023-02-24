a9d78fedb055939b340bd39f8c8b24e511e72b94
Krai53n <greasha46@gmail.com>
2023-02-24 21:09:28 +0300

    fix spaces

--- tests/map-unordered_map.cpp
+++ tests/map-unordered_map.cpp
@@ -15,21 +15,21 @@ namespace t_map {
     srand(seed);
     map<int, int> m;
     for (int i = 0; i < num; i++) {
-	  int tmp = rand() * rand();
-	  m[tmp] = tmp;
-	}
-	return m;
+      int tmp = rand() * rand();
+      m[tmp] = tmp;
+    }
+    return m;
   }
 
   int forward_iteration(int num, int seed) {
-	auto m = create(num, seed);
-	auto start = chrono::steady_clock::now();
-	for (auto i = m.begin(); i != m.end(); i++) {
-	  int tmp = (*i).second;
-	  int res_tmp = tmp * tmp;
-	}
-	auto end = chrono::steady_clock::now();
-	return (end - start) / 1ms;
+    auto m = create(num, seed);
+    auto start = chrono::steady_clock::now();
+    for (auto i = m.begin(); i != m.end(); i++) {
+      int tmp = (*i).second;
+      int res_tmp = tmp * tmp;
+    }
+    auto end = chrono::steady_clock::now();
+    return (end - start) / 1ms;
   }
 
 }; // t_map
@@ -37,24 +37,24 @@ namespace t_map {
 namespace t_umap {
 
   unordered_map<int, int> create(int num, int seed) {
-	srand(seed);
-	unordered_map<int, int> m;
-	for (int i = 0; i < num; i++) {
-	  int tmp = rand() * rand();
-	  m[tmp] = tmp;
-	}
-	return m;
+    srand(seed);
+    unordered_map<int, int> m;
+    for (int i = 0; i < num; i++) {
+      int tmp = rand() * rand();
+      m[tmp] = tmp;
+    }
+    return m;
   }
 
   int forward_iteration(int num, int seed) {
-	auto m = create(num, seed);
-	auto start = chrono::steady_clock::now();
-	for (auto i = m.begin(); i != m.end(); i++) {
-	  int tmp = (*i).second;
-	  int res_tmp = tmp * tmp;
-	}
-	auto end = chrono::steady_clock::now();
-	return (end - start) / 1ms;
+    auto m = create(num, seed);
+    auto start = chrono::steady_clock::now();
+    for (auto i = m.begin(); i != m.end(); i++) {
+      int tmp = (*i).second;
+      int res_tmp = tmp * tmp;
+    }
+    auto end = chrono::steady_clock::now();
+    return (end - start) / 1ms;
   }
 
 }; // t_umap
--- tests/test.hpp
+++ tests/test.hpp
@@ -11,12 +11,12 @@
 int average(int (*func)(int, int), int num, int seed) {
   std::vector<int> vec;
   for (int i = 0; i < 5; i++) {
-	int common_time = 0;
-	while (common_time < 500) {
-	  int tmp = func(num, seed);
-	  common_time += tmp;
-	  vec.push_back(tmp);
-	}
+    int common_time = 0;
+    while (common_time < 500) {
+      int tmp = func(num, seed);
+      common_time += tmp;
+      vec.push_back(tmp);
+    }
   }
   std::sort(vec.begin(), vec.end());
   int _sum = 0;
@@ -25,7 +25,7 @@ int average(int (*func)(int, int), int num, int seed) {
   int slice = size / 10;
   slice = slice == 0 ? 1 : slice;
   for (int i = slice; i < size - slice; i++, count++) {
-	_sum += vec[i];
+    _sum += vec[i];
   }
   return _sum / count;
 }
@@ -37,10 +37,10 @@ void test(
   int (*func)(int, int)
 ) {
   for (int num = 400'000, seed = 333; num <= 52'428'800; num += 300'000, seed++) {
-	file << "collection " << collection << std::endl;
-	file << "operation " << operation << std::endl;
-	file << "elems_num " << num << std::endl;
-	file << "time " << average(func, num, seed) << std::endl;
-	file << std::endl;
+    file << "collection " << collection << std::endl;
+    file << "operation " << operation << std::endl;
+    file << "elems_num " << num << std::endl;
+    file << "time " << average(func, num, seed) << std::endl;
+    file << std::endl;
   }
 }
