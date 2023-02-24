ec38f2754bf8f4d5c227aad47e5a55e18df8dade
kra53n <59062496+kra53n@users.noreply.github.com>
2023-02-24 21:11:10 +0300

    add lib for testing

--- tests/map-unordered_map.cpp
+++ tests/map-unordered_map.cpp
@@ -1,2 +1,65 @@
+#include <map>
+#include <unordered_map>
+
+#include <chrono>
+#include <fstream>
+#include <iostream>
+
+#include "test.hpp"
+
+using namespace std;
+
+namespace t_map {
+
+  map<int, int> create(int num, int seed) {
+    srand(seed);
+    map<int, int> m;
+    for (int i = 0; i < num; i++) {
+      int tmp = rand() * rand();
+      m[tmp] = tmp;
+    }
+    return m;
+  }
+
+  int forward_iteration(int num, int seed) {
+    auto m = create(num, seed);
+    auto start = chrono::steady_clock::now();
+    for (auto i = m.begin(); i != m.end(); i++) {
+      int tmp = (*i).second;
+      int res_tmp = tmp * tmp;
+    }
+    auto end = chrono::steady_clock::now();
+    return (end - start) / 1ms;
+  }
+
+}; // t_map
+
+namespace t_umap {
+
+  unordered_map<int, int> create(int num, int seed) {
+    srand(seed);
+    unordered_map<int, int> m;
+    for (int i = 0; i < num; i++) {
+      int tmp = rand() * rand();
+      m[tmp] = tmp;
+    }
+    return m;
+  }
+
+  int forward_iteration(int num, int seed) {
+    auto m = create(num, seed);
+    auto start = chrono::steady_clock::now();
+    for (auto i = m.begin(); i != m.end(); i++) {
+      int tmp = (*i).second;
+      int res_tmp = tmp * tmp;
+    }
+    auto end = chrono::steady_clock::now();
+    return (end - start) / 1ms;
+  }
+
+}; // t_umap
+
 int main() {
+  std::fstream file("map-unordered_map.txt", fstream::out);
+  test(file, "map", "forward_iteration", t_map::forward_iteration);
 }
