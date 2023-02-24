d990ce09d235dd3c3af04309806100bc1128d7c7
Krai53n <greasha46@gmail.com>
2023-02-24 21:39:21 +0300

    add template using

--- tests/map-unordered_map.cpp
+++ tests/map-unordered_map.cpp
@@ -9,57 +9,35 @@
 
 using namespace std;
 
-namespace t_map {
+#define imap map<int, int>
+#define umap unordered_map<int, int>
 
-  map<int, int> create(int num, int seed) {
-    srand(seed);
-    map<int, int> m;
-    for (int i = 0; i < num; i++) {
-      int tmp = rand() * rand();
-      m[tmp] = tmp;
-    }
-    return m;
+template<typename T>
+T create(int num, int seed) {
+  srand(seed);
+  T m;
+  for (int i = 0; i < num; i++) {
+    int tmp = rand() * rand();
+    m[tmp] = tmp;
   }
+  return m;
+}
 
-  int forward_iteration(int num, int seed) {
-    auto m = create(num, seed);
-    auto start = chrono::steady_clock::now();
-    for (auto i = m.begin(); i != m.end(); i++) {
-      int tmp = (*i).second;
-      int res_tmp = tmp * tmp;
-    }
-    auto end = chrono::steady_clock::now();
-    return (end - start) / 1ms;
+template<typename T>
+int forward_iteration(int num, int seed) {
+  T m = create<T>(num, seed);
+  auto start = chrono::steady_clock::now();
+  for (auto i = m.begin(); i != m.end(); i++) {
+    int tmp = (*i).second;
+    int res_tmp = tmp * tmp;
   }
+  auto end = chrono::steady_clock::now();
+  return (end - start) / 1ms;
+}
 
-}; // t_map
-
-namespace t_umap {
-
-  unordered_map<int, int> create(int num, int seed) {
-    srand(seed);
-    unordered_map<int, int> m;
-    for (int i = 0; i < num; i++) {
-      int tmp = rand() * rand();
-      m[tmp] = tmp;
-    }
-    return m;
-  }
-
-  int forward_iteration(int num, int seed) {
-    auto m = create(num, seed);
-    auto start = chrono::steady_clock::now();
-    for (auto i = m.begin(); i != m.end(); i++) {
-      int tmp = (*i).second;
-      int res_tmp = tmp * tmp;
-    }
-    auto end = chrono::steady_clock::now();
-    return (end - start) / 1ms;
-  }
-
-}; // t_umap
 
 int main() {
   std::fstream file("map-unordered_map.txt", fstream::out);
-  test(file, "map", "forward_iteration", t_map::forward_iteration);
+  test(file, "map",  "forward_iteration", forward_iteration<imap>);
+  test(file, "umap", "forward_iteration", forward_iteration<umap>);
 }
